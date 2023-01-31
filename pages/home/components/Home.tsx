99620b89d12bab13f58e080cb155d4eba04a72c2
Antônio <82171302+antonionetodeveloper@users.noreply.github.com>
2023-01-31 10:16:13 -0300

    create home component

--- pages/home/components/Home.tsx
+++ pages/home/components/Home.tsx
@@ -1,13 +1,67 @@
+import Image from "next/image"
+import styled from "styled-components"
 import { Main } from "../style"
 
 export const Home = () => {
 	return (
-		<div>
+		<div className="changer">
 			<Main>
-				<p>Home</p>
+				<Section>
+					<div className="text">
+						<p>
+							Desenvolvedor de software cursando Ciência da Computação na
+							Universidade Tiradentes (UNIT).
+						</p>
+						<div className="stacks">
+							<p>Next</p>
+							<VerticalLine />
+							<p>React</p>
+							<VerticalLine />
+							<p>Node</p>
+							<VerticalLine />
+							<p>TypeScript</p>
+						</div>
+					</div>
+					<Image
+						src="/myPhoto.png"
+						alt="Foto de Antônio"
+						width={200}
+						height={200}
+					/>
+				</Section>
 			</Main>
 		</div>
 	)
 }
+
+const Section = styled.section`
+	display: flex;
+	align-items: center;
+	gap: 0.5vw;
+
+	div.text {
+		width: "25vw";
+
+		p {
+			margin-bottom: 2vw;
+		}
+
+		div.stacks {
+			display: flex;
+			gap: 1vw;
+			justify-content: center;
+			align-items: center;
+
+			p {
+				margin: 0px;
+			}
+		}
+	}
+`
+
+const VerticalLine = styled.div`
+	border-left: 2px solid white;
+	height: 20px;
+`
 
 export default Home
