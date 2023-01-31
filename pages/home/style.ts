330d181ec756f48054e18c96076cc4eac6571c5c
Antônio <82171302+antonionetodeveloper@users.noreply.github.com>
2023-01-31 08:13:35 -0300

    Update style.ts

--- pages/home/style.ts
+++ pages/home/style.ts
@@ -3,19 +3,19 @@ import styled from "styled-components"
 export const Main = styled.main`
 	animation: slide-in-blurred-top 0.6s cubic-bezier(0.23, 1, 0.32, 1) both;
 
-	@keyframes slide-in-blurred-tl {
+	@keyframes slide-in-blurred-top {
 		0% {
-			-webkit-transform: translate(-1000px, -1000px) skew(80deg, 10deg);
-			transform: translate(-1000px, -1000px) skew(80deg, 10deg);
-			-webkit-transform-origin: 100% 0%;
-			transform-origin: 100% 0%;
+			-webkit-transform: translateY(-1000px) scaleY(2.5) scaleX(0.2);
+			transform: translateY(-1000px) scaleY(2.5) scaleX(0.2);
+			-webkit-transform-origin: 50% 0%;
+			transform-origin: 50% 0%;
 			-webkit-filter: blur(40px);
 			filter: blur(40px);
 			opacity: 0;
 		}
 		100% {
-			-webkit-transform: translate(0, 0) skew(0deg, 0deg);
-			transform: translate(0, 0) skew(0deg, 0deg);
+			-webkit-transform: translateY(0) scaleY(1) scaleX(1);
+			transform: translateY(0) scaleY(1) scaleX(1);
 			-webkit-transform-origin: 50% 50%;
 			transform-origin: 50% 50%;
 			-webkit-filter: blur(0);
