206412e097bfa04d7d85f291661e8cf09d39533e
Antônio <82171302+antonionetodeveloper@users.noreply.github.com>
2023-02-01 20:11:08 -0300

    criar sessão do portifólio

--- pages/home/components/Portfolio.tsx
+++ pages/home/components/Portfolio.tsx
@@ -1,13 +1,96 @@
+import Image from "next/image"
+import styled from "styled-components"
 import { Main } from "../style"
+
+interface PropsType {
+	link: string
+	image: string
+}
 
 export const Portfolio = () => {
 	return (
 		<div className="changer">
 			<Main>
-				<p>Portfolio</p>
+				<Section>
+					<Item
+						image="/portfolio/jessica_bede.png"
+						link="https://jessica-bede.vercel.app"
+					/>
+					<hr />
+					<Item
+						image="/portfolio/netflix_clone.png"
+						link="https://github.com/antonionetodeveloper/netflix-clone"
+					/>
+					<hr />
+					<Item
+						image="/portfolio/escolinha.png"
+						link="https://learning-next-beryl.vercel.app"
+					/>
+					<hr />
+					<Item
+						image="/portfolio/CRUD.png"
+						link="https://github.com/antonionetodeveloper/CRUD"
+					/>
+					<hr />
+					<Item image="/portfolio/site2it.png" link="https://site2it.com" />
+					<hr />
+					<Item
+						image="/portfolio/login_azure.png"
+						link="https://github.com/antonionetodeveloper/login_ui"
+					/>
+				</Section>
 			</Main>
 		</div>
 	)
 }
+const Section = styled.section`
+	height: 80vh;
+	display: flex;
+	flex-direction: column;
+	overflow-y: scroll;
+	gap: 2vw;
+
+	hr {
+		width: 90%;
+	}
+`
+
+const Item = ({ link, image }: PropsType) => {
+	return (
+		<ItemContent>
+			<Image src={image} alt="projeto exemplo" width={300} height={200} />
+			<a href={link} target="_blank" rel="noreferrer">
+				saiba mais{" "}
+				<img src="/linkIcon.png" alt="Ícone de link" className="linkIcon" />
+			</a>
+		</ItemContent>
+	)
+}
+const ItemContent = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: space-evenly;
+	gap: 2vw;
+
+	a {
+		width: 20%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-size: 1.5vw;
+		gap: 0.5vw;
+		text-decoration: none;
+		color: #cccccc;
+
+		:hover {
+			cursor: pointer;
+			text-decoration: underline;
+		}
+
+		img.linkIcon {
+			width: 15%;
+		}
+	}
+`
 
 export default Portfolio
