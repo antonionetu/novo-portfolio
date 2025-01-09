e5c1a5afd2aba491d1d75dd125b62fa31fbb17cc
Antônio Fernandes <devantonio.fer@gmail.com>
2025-01-09 01:28:45 -0300

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<link rel="preconnect" href="https://fonts.googleapis.com" />
-			<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+			<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
 			<link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
 			<GlobalStyle />
 			<Component {...pageProps} />
